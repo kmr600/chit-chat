4d92ae04ac7cbe3d015b5bb108c39fff804e82b7
kmr600 <ksmith600@gmail.com>
2020-03-27 22:45:29 -0700

    Add helmet and static files to server

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const http = require("http");
 const express = require("express");
+const helmet = require("helmet");
 
 const sockets = require("./sockets");
 
 const app = express();
 const server = http.createServer(app);
 sockets(server);
+
+// Set security headers
+app.use(helmet());
+
+// Serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
